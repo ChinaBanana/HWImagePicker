ccf70cf22e467ccf9c7c566eaecce9ec250082e5
ChinaBanana <cocowu@CocodeMacBook.local>
2017-06-22 16:42:10 +0800

    fix
    fix

---
+++ ImagePickerDemoTests/ImagePickerDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  ImagePickerDemoTests.m
+//  ImagePickerDemoTests
+//
+//  Created by Coco Wu on 2017/6/19.
+//  Copyright © 2017年 cyt. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ImagePickerDemoTests : XCTestCase
+
+@end
+
+@implementation ImagePickerDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
